6d53df6f6a012d6b34bd877e31ffe7a56029e9ef
Lupitasari Silitonga <2381017@unai.edu>
2024-09-05 22:22:16 +0700

    Handson2

--- latihansoal/soal1.java
+++ latihansoal/soal1.java
@@ -1,5 +1,24 @@
 package latihansoal;
 
+import java.util.Scanner;
+
 public class soal1 {
-    //membuat nilai maksimum dari 2 bilangan
+    public static void main(String[] args) {
+        int[] bilangan = new int[2];
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan Bilangan Pertama: ");
+        bilangan[0] = input.nextInt();
+        System.out.println("Masukkan Bilangan Kedua: ");
+        bilangan[1] = input.nextInt();
+
+        if (bilangan[1] > bilangan[0]) {
+            System.out.println("Bilangan terbesar adalah: " + bilangan[1]);
+        } else if (bilangan[0] > bilangan[1]) {
+            System.out.println("Bilangan terbesar adalah: " + bilangan[0]);
+        } else {
+            System.out.println("Kedua bilangan sama besar.");
+        }
+
+        input.close();
+    }
 }
--- latihansoal/soal2.java
+++ latihansoal/soal2.java
@@ -1,5 +1,29 @@
 package latihansoal;
+import java.util.Scanner;
 
 public class soal2 {
-    //membuat nilai maksimum dari 3 bilangan
-}
+
+        public static void main(String[] args) {
+            int[] bilangan = new int[3];
+            Scanner input = new Scanner(System.in);
+
+            System.out.println("Masukkan Bilangan Pertama: ");
+            bilangan[0] = input.nextInt();
+
+            System.out.println("Masukkan Bilangan Kedua: ");
+            bilangan[1] = input.nextInt();
+
+            System.out.println("Masukkan bilangan ketiga");
+            bilangan[2] = input.nextInt();
+
+            if (bilangan[0] >= bilangan[1] && bilangan [0] >= bilangan[2]) {
+                System.out.println("Bilangan terbesar adalah: " + bilangan[0]);
+            } else if (bilangan[1] >= bilangan[0] && bilangan[1] >= bilangan [2]) {
+                System.out.println("Bilangan terbesar adalah: " + bilangan[1]);
+            } else {
+                System.out.println("Bilangan terbesar adalah : " + bilangan[2]);
+            }
+
+            input.close();
+        }
+    }
